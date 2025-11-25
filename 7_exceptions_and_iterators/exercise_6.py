162462fba19aac4e91a30f2fffcdf85c7943a79d
AlexNordahl <nordahlalex@gmail.com>
2025-11-25 17:37:41 +0100

    changed exercise_6 to be more idiomatic

--- 7_exceptions_and_iterators/exercise_6.py
+++ 7_exceptions_and_iterators/exercise_6.py
@@ -5,27 +5,31 @@ class ZeroOne:
     def __init__(self):
         self.flag = False
 
-    def __call__(self):
-        if self.flag == False:
-            self.flag = True
-            return 0
-        
-        self.flag = False
-        return 1
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return int(not self.flag)
 
 class RandomDir:
     def __init__(self):
         self.dirs = ['N', 'E', 'S', 'W']
 
-    def __call__(self):
-        return self.dirs[random.randint(0, len(self.dirs) - 1)]
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return random.choice(self.dirs)
 
 class NextDay():
     def __init__(self):
         self.days = [0, 1, 2, 3, 4, 5, 6]
         self.cur = -1
+
+    def __iter__(self):
+        return self
     
-    def __call__(self):
+    def __next__(self):
         self.cur += 1
 
         if self.cur == len(self.days):
